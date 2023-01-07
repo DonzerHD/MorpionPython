009aae8c6ecfe9c3b90d1f70e25c157484ec19da
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-07 21:45:53 +0100

    grille

---
+++ main.py
@@ -0,0 +1,12 @@
+grille_morpion = [[' ',' ',' '],
+                  [' ',' ',' '],
+                  [' ',' ',' ']]
+
+
+
+def grille():
+  for ligne in grille_morpion:
+    print('¦'.join(ligne))
+    
+grille()
+    
