e0058fa10a234d650ee68e5ad90b0601a362e6e2
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-08 11:53:09 +0100

    colonne

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ grille_morpion = [[' ',' ',' '],
 
 
 def grille():
-  for ligne in grille_morpion:
+  print("  1 2 3")
+  for i, ligne in enumerate(grille_morpion):
+    print(i+1, end=' ')
     print('|'.join(ligne))
  
    
-def joueur(signe):
+def joueurSaisie(signe):
      ligne = int(input("Saisissez la ligne 1 ou 2 ou 3 où vous voulez jouer: ")) - 1
      colonne = int(input("Saisissez la colonne 1 ou 2 ou 3 où vous voulez jouer: ")) - 1
      if grille_morpion[ligne][colonne] != ' ':
@@ -18,10 +20,17 @@ def joueur(signe):
          grille_morpion[ligne][colonne] = signe
 
     
-def jouer(signe):
+def verification(signe):
+   #ligne
    for ligne in grille_morpion:
      if ligne == [signe , signe , signe]:
        return True
+     #colonne
+   for colonne in range(3):
+     if grille_morpion[0][colonne] == signe and grille_morpion[1][colonne] and grille_morpion[2][colonne]:
+       return True
+     #diagonale
+   
      
      
      
