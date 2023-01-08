80390c4bd44048c73604d75964366d163a559f5d
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-08 10:50:43 +0100

    test

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ grille_morpion = [[' ',' ',' '],
 
 def grille():
   for ligne in grille_morpion:
-    print('¦'.join(ligne))
+    print('|'.join(ligne))
+ 
+   
+def joueur(signe):
+     ligne = int(input("Saisissez la ligne 1 ou 2 ou 3 où vous voulez jouer: ")) - 1
+     colonne = int(input("Saisissez la colonne 1 ou 2 ou 3 où vous voulez jouer: ")) - 1
+     if grille_morpion[ligne][colonne] != ' ':
+         print('case déja pris')
+     else:
+         grille_morpion[ligne][colonne] = signe
+
     
+def jouer(signe):
+   for ligne in grille_morpion:
+     if ligne == [signe , signe , signe]:
+       return True
+     
+     
+     
+
+
+
 grille()
-    
---
+++ test.py
@@ -0,0 +1,13 @@
+test = [['1', '2' ,'3'],
+        ['4', '5' ,'6'],
+        ['7', '8' ,'9']]
+
+for colonne in range(3):
+    if test[0][colonne] == '1' and test[1][colonne] == '4' and test[2][colonne] == '7':
+        print('bien')
+    else:
+        print('pas bien')
+    
+     
+      
+     
